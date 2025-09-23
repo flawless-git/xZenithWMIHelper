6ea3fe59bf6b576a6cffb0d88ed7c36348153a84
arifpebryan22@gmail.com <arifpebryan22@gmail.com>
2025-09-23 18:17:34 +0700

    init project

--- obj/Debug/net9.0/xZenithWMIHelper.AssemblyInfo.cs
+++ obj/Debug/net9.0/xZenithWMIHelper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xZenithWMIHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505d8b75004c98e6d1bdf0a08175128e3ad7f843")]
 [assembly: System.Reflection.AssemblyProductAttribute("xZenithWMIHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xZenithWMIHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
